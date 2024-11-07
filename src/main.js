7827d5c60d4ae29dbc2301fa7fc65a6400b0a135
Sebastian <Sebastiandd87@gmail.com>
2024-11-06 19:44:10 -0500

    Initial Commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';        
+import App from './App.vue';             
+import router from './router';       
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser } from '@fortawesome/free-solid-svg-icons'; 
+import { faKey } from '@fortawesome/free-solid-svg-icons'; 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';    
 
-createApp(App).mount('#app')
+library.add(faUser);
+library.add(faKey); 
+
+const app = createApp(App);      
+app.component('font-awesome-icon', FontAwesomeIcon); 
+app.use(router); 
+app.mount('#app'); 
